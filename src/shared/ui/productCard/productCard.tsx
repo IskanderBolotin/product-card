d10182d57c71c0d81765f8490310b5d0f7621605
Bolotin Alexander <sanya.bolotin.96@mail.ru>
2025-06-09 20:17:47 +0300

    add price formatter

--- src/shared/ui/productCard/productCard.tsx
+++ src/shared/ui/productCard/productCard.tsx
@@ -1,10 +1,21 @@
-import type { ProductDTO } from "@/shared/model";
+import type { CurrencyType, ProductDTO } from "@/shared/model";
 import s from "./productCard.module.scss";
 
 type Props = {
-  data?: ProductDTO
+  data?: ProductDTO;
 };
 
+const getPrice = (price: number, currency: CurrencyType) => {
+  const priceInRubles = price / 100;
+  
+  const formatter = new Intl.NumberFormat("ru-RU", {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  return formatter.format(priceInRubles);
+};
 const ProductCard: React.FC<Props> = ({ data }) => {
   const { title, imageSrc, price } = data || {};
 
@@ -12,26 +23,16 @@ const ProductCard: React.FC<Props> = ({ data }) => {
     <div className={s.card}>
       <div className={s.image}>
         <div className={s.inner}>
-          {
-            title !== undefined &&
-            <img src={imageSrc} alt={title} />
-          }
+          {title !== undefined && <img src={imageSrc} alt={title} />}
         </div>
       </div>
-      <h3 className={s.title}>
-        {
-          title !== undefined ?
-          title:
-          "Загрузка..."
-        }
-      </h3>
+      <h3 className={s.title}>{title !== undefined ? title : "Загрузка..."}</h3>
       <div className={s.price}>
-        {
-          price !== undefined &&
+        {price !== undefined && (
           <>
-            <span>{price?.price}</span>&nbsp;<span>{price?.currency}</span>
+            <span>{getPrice(price?.price, price?.currency)}</span>
           </>
-        }
+        )}
       </div>
     </div>
   );
